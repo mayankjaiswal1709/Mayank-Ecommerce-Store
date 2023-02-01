955425861f6bd6a97b06df004f8ce03c9d2fbfbe
Mayank Jaiswal <mayankjaiswal20180@gmail.com>
2023-02-01 15:12:45 +0530

    services.js added

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,19 +1,46 @@
 import styled from "styled-components";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdSecurity } from "react-icons/md";
+import { GiReceiveMoney } from "react-icons/gi";
+import { RiSecurePaymentLine } from "react-icons/ri";
 
 const Services = () => {
-    return <Wrapper>
-
-        <div className="container">
-            <div className="grid grid-three-column">
-                <div className="services-1">
-                    <div>
-                        <TbTruckDelivery classname="icon" />
-                    </div>
-                </div>
-
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="grid grid-three-column">
+          <div className="services-1">
+            <div>
+              <TbTruckDelivery className="icon" />
+              <h3>Super Fast and Free Delivery</h3>
             </div>
+          </div>
+
+          <div className="services-2">
+            <div className="services-colum-2">
+              <div>
+                <MdSecurity className="icon" />
+                <h3>Non-contact Shipping</h3>
+              </div>
+            </div>
+            <div className="services-colum-2">
+              <div>
+                <GiReceiveMoney className="icon" />
+                <h3>Money-back Guaranteed</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="services-3">
+            <div>
+              <RiSecurePaymentLine className="icon" />
+              <h3>Super Secure Payment System</h3>
+            </div>
+          </div>
         </div>
-    </Wrapper>;
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
